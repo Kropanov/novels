61f3f93c2d023f87f9517e8e258a293953eb9a36
Kropanov Maksim <kropanov2012@gmail.com>
2022-05-03 10:35:53 +0300

    Creating text in a footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,41 @@ import classes from './Footer.module.scss'
 const Footer = props => {
     return (
         <footer className={classes.Footer}>
-        
+            <div class="container-fluid !direction !spacing">
+                <div class="row">
+                    <div class="col-3">
+                        <ul>
+                            <span>Novels</span>
+                            <li>Сайт может содержать материалы, не предназначенные для просмотра лицами, не достигшими 18 лет!</li>
+                            <li>— Используя данный сайт, вы подтверждаете, что вам уже исполнилось 18 лет.</li>
+                        </ul>
+                    </div>
+                    <div class="col-3">
+                        <ul>
+                            <span>Ссылки</span>
+                            <li>Главная</li>
+                            <li>Новеллы</li>
+                            <li>Манга</li>
+                            <li>Блог</li>
+                        </ul>
+                    </div>
+                    <div class="col-3 ">
+                        <ul>
+                            <span>Помощь</span>
+                            <li>FAQ</li>
+                            <li>Журнал модерации</li>
+                            <li>Правила</li>
+                        </ul>
+                    </div>
+                    <div class="col-3 ">
+                        <ul>
+                            <span>Контакты</span>
+                            <li>Для правообладателей</li>
+                            <li>Обратная связь</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </footer>
     )
 }

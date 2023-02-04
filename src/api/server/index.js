f913ae85e0e7f4c399f7d1bfe473eeeca7b02a42
Maxim Kropanov <kropanov2012@gmail.com>
2023-02-04 22:21:20 +0300

    Connection our MongoDB server

--- src/api/server/index.js
+++ src/api/server/index.js
@@ -1,16 +1,25 @@
 const dotenv = require('dotenv').config({ path: "./config.env" });
-const fs = require('fs');
+const mongoose = require('mongoose');
 const express = require('express');
 
 const app = express();
 
-console.log(process.env.PORT)
+const DB = process.env.DATABASE.replace(
+    "<password>",
+    process.env.DATABASE_PASSWORD
+);
 
-const PORT = process.env.PORT || 3001;
+mongoose.set('strictQuery', true);
+mongoose.connect(DB)
+    .then(con => {
+        console.log('Connected!')
+    });
 
 app.get("/novels", (req, res) => {
     res.json({ message: "Hello from Express!" });
 });
+
+const PORT = process.env.PORT || 3001;
 
 app.listen( PORT, () => {
     console.log( `Server listening on ${PORT}...` );
